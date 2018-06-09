ad369a26e89e60ec3f9ecf21390b6b0d16680d2d
franklinokech <franklinokech@gmail.com>
2018-06-09 13:32:57 +0300

    Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# isrp
-ICTA Standards and Requirements Portal is A one stop search engine for governmenet system admins and general public to search all the minimum requirements for government ICT products and services
---
+++ application/controllers/Register.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Register extends CI_Controller {
+
+    public function registerUser() {
+
+        //validate  the data taken through the register form
+        $this->form_validation->set_rules('username','Username','required|is_unique[users.username]');
+        $this->form_validation->set_rules('fname','Full Name','required');
+        $this->form_validation->set_rules('email','Email','required|valid_email');
+        $this->form_validation->set_rules('mobile','Mobile Number','required');
+        $this->form_validation->set_rules('password','Password','required');
+        $this->form_validation->set_rules('cpassword','Confirm Password','required|matches[password]');
+
+        if ($this->form_validation->run() == TRUE) {
+
+        //load the model to connect to the db
+        $this->load->model('Model_user');
+        $this->Model_user->insertUser();
+
+        //set message to be shown when registration is completed
+        $this->session->set_flashdata('success','Registration Is Successful!');
+        redirect('Home/Register');
+
+    } else {
+
+        $this->load->view('register_view');
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
