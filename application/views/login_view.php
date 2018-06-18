817e5c1be4ad0e66801e8f082772246cdddd6dd1
franklinokech <franklinokech@gmail.com>
2018-06-18 11:46:18 +0300

    added the icta logo on login page

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -16,6 +16,7 @@
             <div class='col-md-4'>
                 <div class='panel panel-default'>
                     <div class='panel-body'>
+                        <img src="<?php echo base_url('assets/img/logo.jpeg');?>"
                         <!-- validation message for a successful login -->
                         <?php if ($this->session->flashdata('error')) {?>
                             <div class="alert alert-danger alert-dismissible" role="alert">
@@ -32,7 +33,7 @@
                         ?>
 
                         <?php echo form_open('Login/loginUser') ?>
-
+                       
                             <div class="form-group">
                                 <label for="username">Username</label>
                                 <input type="text" class="form-control" name="username" id="username" placeholder="Username">
